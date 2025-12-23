af26de7e872072717c8ed167091f44fae5f7fbf4
Biniyam <myrita099@gmail.com>
2025-12-23 14:35:57 +0300

    refactor(header): merge nav into header component
    Removed standalone Nav component and integrated its links directly into the Header.
    The navigation was too minimal to justify a separate component, so merging improves
    structure, reduces overhead, and keeps layout more cohesive.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,34 @@
 import Logo from "../../assets/Logo.svg";
-import Nav from "../nav/Nav";
 import "./Header.css";
 
 function Header() {
-    return(
-        <header className="header">               
-            <img src={Logo} alt="Little Lemon logo" />
-            <Nav />
-        </header>
-    )
-};
+  return (
+    <header className="header">
+      <img src={Logo} alt="Little Lemon logo" className="logo"/>
+      <nav className="nav">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Reservations</a>
+          </li>
+          <li>
+            <a href="#">Order online</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
