ae6d328f4f436ca6da85575f0e087cc15e6bc8b8
Biniyam <myrita099@gmail.com>
2025-12-29 14:35:18 +0300

    feat(router): configure React Router v7 with nested routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import Layout from "./components/layout/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/Header";
+import Main from "./main/Main";
+import Footer from "./components/footer/Footer";
+import PaymentInfo from "./main/reservation/PaymentInfo";
+import ReservationInfo from "./main/reservation/ReservationInfo";
+import PersonalInfo from "./main/reservation/PersonalInfo";
+import Reservation from "./main/Reservation";
 
 function App() {
   return (
-    <>
-      <Layout />
-    </>
+    <div className="app">
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="reservation" element={<Reservation />}>
+            <Route path="paymentInfo" element={<PaymentInfo />} />
+            <Route path="personalInfo" element={<PersonalInfo />} />
+            <Route path="reservationInfo" element={<ReservationInfo />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
