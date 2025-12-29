3fc8a825c2c9fa75006238adc9991ef6fc1d5684
Biniyam <myrita099@gmail.com>
2025-12-29 15:50:31 +0300

    feat(reservation): add Reservation.js as parent container for reservation components

--- src/main/Reservation.js
+++ src/main/Reservation.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Back from "../assets/back.svg";
 
 function Reservation() {
-  return <div>Reservation</div>;
+  return (
+    <div>
+      <button>
+        <img src={Back} alt="back icon" style={{ width: "20px" }} />
+        Back
+      </button>
+      <h2>Reservation</h2>
+      <Outlet />
+    </div>
+  );
 }
 
 export default Reservation;
