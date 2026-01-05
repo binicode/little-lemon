1fb27fb92b7032c2afb67a2ca918ba8e8cba2148
Biniyam <myrita099@gmail.com>
2026-01-05 16:21:18 +0300

    feat(reservation): add JS interactivity to ReservationInfo form

--- src/main/reservation/PersonalInfo.js
+++ src/main/reservation/PersonalInfo.js
@@ -1,75 +1,72 @@
 import React from "react";
 
-export default function PersonalInfo() {
+export default function PersonalInfo({
+  formData,
+  handleChange,
+  pervStep,
+  nextStep,
+}) {
   return (
     <div>
       <div className="form-container">
         <div className="welcome-text">
           <h3>{`Please provide your information here.`}</h3>
         </div>
-        <form className="reservation-form">
-          {/* First name */}
-          <div className="form-group">
-            <label for="first-name">First Name</label>
-            <input
-              type="text"
-              name="first-name"
-              id="first-name"
-              placeholder="Enter your first name"
-              className="custom-input"
-              // value={formData.dinners}
-              // onChange={handleChange}
-              required
-            />
-          </div>
 
-          {/* Last name */}
-          <div className="form-group">
-            <label for="last-name">Last Name</label>
-            <input
-              type="text"
-              name="last-name"
-              id="last-name"
-              placeholder="Enter your last name"
-              className="custom-input"
-              // value={formData.dinners}
-              // onChange={handleChange}
-              required
-            />
-          </div>
+        {/* First name */}
+        <div className="form-group">
+          <label for="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Enter your first name"
+            className="custom-input"
+            value={formData.personal.name}
+            onChange={(e) => handleChange("personal", "name", e.target.value)}
+            required
+          />
+        </div>
 
-          {/* Email */}
-          <div className="form-group">
-            <label for="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter your email"
-              className="custom-input"
-              // value={formData.dinners}
-              // onChange={handleChange}
-              required
-            />
-          </div>
+        {/* Email */}
+        <div className="form-group">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+            className="custom-input"
+            // value={formData.personal.email}
+            // onChange={e =>
+            // handleChange("reservation", "personal", e.target.value)}
+            required
+          />
+        </div>
 
-          {/* Password */}
-          <div className="form-group">
-            <label for="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Enter number"
-              className="custom-input"
-              // value={formData.dinners}
-              // onChange={handleChange}
-              required
-            />
-          </div>
-
-          <button type="submit">Reserve Table</button>
-        </form>
+        {/* Password */}
+        <div className="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter number"
+            className="custom-input"
+            // value={formData.personal.password}
+            // onChange={e =>
+            // handleChange("personal", "password", e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="button" onClick={pervStep}>
+            Back
+          </button>
+          <button type="button" onClick={nextStep}>
+            Reserve Table
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/main/reservation/ReservationInfo.js
+++ src/main/reservation/ReservationInfo.js
@@ -1,79 +1,84 @@
 import React from "react";
-// import React, { useState } from "react";
 
-function ReservationInfo() {
-  // { const [formData, setFormData] = useState({ date: "", dinners: 1, seating: "indoor", }); const handleChange = (e) => { const { name, value } = e.target; setFormData((prev) => ({ ...prev, [name]: value, })); }; const handleSubmit = (e) => { e.preventDefault(); console.log("Form submitted:", formData); };
+function ReservationInfo({ formData, handleChange, nextStep }) {
   return (
     <div className="form-container">
       <div className="welcome-text">
         <h2>Welcome!</h2>
         <p>{`Let's save you the perfect spot.`}</p>
       </div>
-      <form className="reservation-form">
-        {/* Date field */}
-        <div className="form-group">
-          <label for="date"></label>
-          Date
-          <input
-            type="date"
-            name="date"
-            id="date"
-            placeholder="Select a date"
-            className="custom-input"
-            // value={formData.date}
-            // onChange={handleChange}
-            required
-          />
-        </div>
 
-        {/* Number of dinners */}
-        <div className="form-group">
-          <label for="dinners">Dinners</label>
-          <input
-            type="number"
-            name="dinners"
-            id="dinners"
-            placeholder="Enter number"
-            className="custom-input"
-            min="1"
-            max="10"
-            // value={formData.dinners}
-            // onChange={handleChange}
-            required
-          />
-        </div>
+      {/* Date field */}
+      <div className="form-group">
+        <label htmlFor="date">Date</label>
 
-        {/* Radio buttons for seating */}
-        <div className="form-group-radio">
-          <legend>Seating option</legend>
-          <div className="radio">
-            <label for="indoor">
-              <input
-                type="radio"
-                name="seating"
-                value="indoor"
-                id="indoor"
-                // checked={formData.seating === "indoor"}
-                // onChange={handleChange}
-              />
-              Indoor
-            </label>
-            <label for="outdoor">
-              <input
-                type="radio"
-                name="seating"
-                value="outdoor"
-                id="outdoor"
-                // checked={formData.seating === "outdoor"}
-                // onChange={handleChange}
-              />
-              Outdoor
-            </label>
-          </div>
-        </div>
+        <input
+          type="date"
+          name="date"
+          id="date"
+          placeholder="Select a date"
+          className="custom-input"
+          value={formData.reservation.date}
+          onChange={(e) => handleChange("reservation", "date", e.target.value)}
+          required
+        />
+      </div>
 
-        <button type="submit">Let's go</button>
-      </form>
+      {/* Number of dinners */}
+      <div className="form-group">
+        <label for="diners">Diners</label>
+        <input
+          type="number"
+          name="diners"
+          id="diners"
+          placeholder="Enter number"
+          className="custom-input"
+          min="1"
+          max="10"
+          value={formData.reservation.diners}
+          onChange={(e) =>
+            handleChange("reservation", "diners", e.target.value)
+          }
+          required
+        />
+      </div>
+
+      {/* Radio buttons for seating */}
+      <div className="form-group-radio">
+        <legend>Seating option</legend>
+        <div className="radio">
+          <label for="indoor">
+            <input
+              type="radio"
+              name="seating"
+              value="indoor"
+              id="indoor"
+              checked={formData.reservation.seating === "indoor"}
+              onChange={(e) =>
+                handleChange("reservation", "seating", e.target.value)
+              }
+            />
+            Indoor
+          </label>
+          <label for="outdoor">
+            <input
+              type="radio"
+              name="seating"
+              value="outdoor"
+              id="outdoor"
+              checked={formData.reservation.seating === "outdoor"}
+              onChange={(e) =>
+                handleChange("reservation", "seating", e.target.value)
+              }
+            />
+            Outdoor
+          </label>
+        </div>
+      </div>
+
+      <button type="button" onClick={nextStep}>
+        Let's go
+      </button>
     </div>
   );
 }
