635699324c49893d2a9eefdefd746d31767a8e46
Biniyam <myrita099@gmail.com>
2026-01-21 14:53:40 +0300

    refactor: adjust payment form input types

--- src/main/reservation/PaymentInfo.js
+++ src/main/reservation/PaymentInfo.js
@@ -1,76 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import "../Reservation.css";
+import CreditCard from "../../assets/creditcard.svg";
 
-export default function PaymentInfo() {
+export default function PaymentInfo({
+  formData,
+  handleChange,
+  handleFinalSubmit,
+}) {
   return (
     <div>
-      PaymentInfo
       <div className="form-container">
         <div className="welcome-text">
           <h3>{`Please provide your information here.`}</h3>
         </div>
-        <form className="reservation-form">
-          {/* First name */}
+
+        {/* Card number */}
+        <div className="form-group">
+          <label for="card-number">Card Number</label>
+          <input
+            type="number-number-number-number"
+            name="card-number"
+            id="card-number"
+            placeholder="xxxx-xxxx-xxxx-xxxx"
+            className="custom-input"
+            value={formData.payment.cardNumber}
+            onChange={(e) =>
+              handleChange("payment", "cardNumber", e.target.value)
+            }
+            required
+          />
+        </div>
+
+        <div className="card-details">
           <div className="form-group">
-            <label for="first-name">First Name</label>
+            <label for="exp-date">Exp Date</label>
+            <input
+              type="number/number"
+              name="exp-date"
+              id="exp-date"
+              placeholder="MM/YY"
+              className="custom-input"
+              value={formData.payment.expiry}
+              onChange={(e) =>
+                handleChange("payment", "expiry", e.target.value)
+              }
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label for="cvv">CVV</label>
             <input
               type="text"
-              name="first-name"
-              id="first-name"
-              placeholder="Enter your first name"
+              name="cvv"
+              id="cvv"
+              placeholder="123"
               className="custom-input"
-              // value={formData.dinners}
-              // onChange={handleChange}
+              minLength={3}
+              maxLength={3}
+              value={formData.payment.cvv}
+              onChange={(e) => handleChange("payment", "cvv", e.target.value)}
               required
             />
           </div>
+          <button className="credit-card-icon">
+            <img src={CreditCard} alt="credit card icon" />
+          </button>
+        </div>
 
-          {/* Last name */}
-          <div className="form-group">
-            <label for="last-name">Last Name</label>
-            <input
-              type="text"
-              name="last-name"
-              id="last-name"
-              placeholder="Enter your last name"
-              className="custom-input"
-              // value={formData.dinners}
-              // onChange={handleChange}
-              required
-            />
-          </div>
-
-          {/* Number of dinners */}
-          <div className="form-group">
-            <label for="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Enter your email"
-              className="custom-input"
-              // value={formData.dinners}
-              // onChange={handleChange}
-              required
-            />
-          </div>
-
-          {/* Number of dinners */}
-          <div className="form-group">
-            <label for="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Enter number"
-              className="custom-input"
-              // value={formData.dinners}
-              // onChange={handleChange}
-              required
-            />
-          </div>
-
-          <button type="submit">Reserve Table</button>
-        </form>
+        <button type="submit">Confirm Reservation</button>
       </div>
     </div>
   );
