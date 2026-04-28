c2b3b7e0222fe429463b2919b6f4da9a125d67ef
biniyam <myrita099@gmail.com>
2026-04-28 12:20:39 +0300

    chore: update import paths in App.js
    - Adjusted Reservation component import to reflect new structure
    - Ensured correct module resolution after refactor

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
-import ReservationInfo from "./components/main/ReservationInfo";
-import Reservation from "./components/main/Reservation";
+import ReservationInfo from "./components/main/BookingForm";
+import Reservation from "./components/main/BookingPage";
 
 function App() {
   return (
