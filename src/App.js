cacd6d29f22e6879f4c5ad2ebce28e155b5fbe4e
biniyam <myrita099@gmail.com>
2026-05-03 13:32:04 +0300

    refactor: lift booking state to App.js
    - Moved availableTimes state from BookingForm.js to App.js
    - Passed state and updater down to BookingForm via props

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
-import BookingPage from "./components/main/BookingPage";
+import BookingForm from "./components/main/BookingForm";
+import { useState } from "react";
 
 function App() {
+
+    const [availableTimes, setAvailableTimes] = useState([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00"
+  ]);
+
   return (
     <div className="app">
       <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="reservation" element={<BookingPage />}></Route>
+          <Route path="reservation" element={<BookingForm
+            availableTimes={availableTimes}
+            setAvailableTimes={setAvailableTimes}
+           />} ></Route>
         </Routes>
       </BrowserRouter>
       <Footer />
